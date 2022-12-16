e549c6f1bc52691999021a2991a93917c9018022
harry-ramsay2000 <78567606+harry-ramsay2000@users.noreply.github.com>
2022-12-16 17:18:42 +0000

    update index

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-const FeatureList = [
-];
-
 function Feature({Svg, title, description}) {
   return (
     <div className={clsx('col col--4')}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,9 +26,9 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
-      <main>
+      {/* <main>
         <HomepageFeatures />
-      </main>
+      </main> */}
     </Layout>
   );
 }
