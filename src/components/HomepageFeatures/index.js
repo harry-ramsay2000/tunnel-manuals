a839d74dcf421210bacf791f704d694d6d67f4e6
harry-ramsay2000 <78567606+harry-ramsay2000@users.noreply.github.com>
2022-12-16 17:15:29 +0000

    update index

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -18,17 +18,3 @@ function Feature({Svg, title, description}) {
     </div>
   );
 }
-
-export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
-}
